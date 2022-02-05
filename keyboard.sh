d833116fee8778e975a4218f760571dd3a23540f
Baby Dragon <94061352+MarMu-BabyDragon@users.noreply.github.com>
2022-02-05 20:50:06 +0630

    Create keyboard.sh

---
+++ keyboard.sh
@@ -0,0 +1,259 @@
+#Creation By MarMu
+
+#Facebook Page - Technology By MarMu
+
+#DO NOT THIEF MY STYLE
+
+#DON'T COPY & MODIFY THIS CODE
+
+####£££££££££££££££££££££££££################
+
+#-command_not_found_handle() {
+
+#	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"#}
+
+clear
+
+RD='\e[1;31m'
+
+GR='\e[1;32m'
+
+YL='\e[1;33m'
+
+BL='\e[1;34m'
+
+PP='\e[1;35m'
+
+LBL='\e[1;36m'
+
+GY='\e[1;30m'
+
+WH='\e[1;37m'
+
+clear
+
+figlet setup | lolcat
+
+#pkg installation
+
+pkg install ruby -y
+
+pkg install python -y
+
+pkg install figlet -y
+
+gem install lolcat
+
+pkg install toilet -y
+
+echo
+
+clear
+
+echo " "
+
+echo " "
+
+echo -e "\e[1m\e[31m\
+
+     ████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗ \e[1;91m
+
+     ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝ \e[1;97m
+
+     ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░ \e[1;97m
+
+     ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░ \e[1;91m
+
+     ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗ \e[1;91m
+
+     ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝
+
+\e[36m\
+
+                     
+
+          ╭╮╭━┳━━━┳╮╱╱╭┳━━╮╭━━━┳━━━┳━━━┳━━━╮\e[1;36m
+
+          ┃┃┃╭┫╭━━┫╰╮╭╯┃╭╮┃┃╭━╮┃╭━╮┃╭━╮┣╮╭╮┃ \e[1;35m
+
+          ┃╰╯╯┃╰━━╋╮╰╯╭┫╰╯╰┫┃╱┃┃┃╱┃┃╰━╯┃┃┃┃┃ \e[1;35m
+
+          ┃╭╮┃┃╭━━╯╰╮╭╯┃╭━╮┃┃╱┃┃╰━╯┃╭╮╭╯┃┃┃┃ \e[1;34m
+
+          ┃┃┃╰┫╰━━╮╱┃┃╱┃╰━╯┃╰━╯┃╭━╮┃┃┃╰┳╯╰╯┃\e[1;34m
+
+          ╰╯╰━┻━━━╯╱╰╯╱╰━━━┻━━━┻╯╱╰┻╯╰━┻━━━╯\e[1;92m
+
+"
+
+echo 
+
+sleep 2
+
+pkg install ruby 
+
+gem install lolcat
+
+echo " "
+
+echo -e $GR"                 ☆☆☆ ($YL  M A R M U  $GR) ☆☆☆"
+
+echo " "
+
+echo -e $GY"           《$GR T$gYL E$RD R$GY M$LBL U$GR X$WH  ⭐ $BL K$RD E$BL Y$GR B$PP O$LBL A$YL R$BL D$GY 》" 
+
+echo
+
+echo -e ""
+
+echo -e $GR " Enter Your Name : \e[1;35m "
+
+read name
+
+toilet -F gay -f slant "$name"  
+
+echo -e "   Your Termux Keyboard Has Upgraded" | lolcat    
+
+sleep 2
+
+    
+
+echo " "
+
+termux-setup-storage
+
+pip2 install lolcat
+
+pkg install figlet -y
+
+echo ""
+
+clear
+
+echo "                   DO YOU KNOW MARMU ??"  | lolcat
+
+pkg install toilet -y
+
+cd $HOME
+
+sleep 2
+
+echo -e "\e[1m\e[35m\
+
+000000000000000_000000000000000
+
+00000000000000___00000000000000
+
+0000000000000_____0000000000000
+
+000000000000_______000000000000
+
+00000000000_________00000000000
+
+0__________ MYANMAR _________00
+
+000_______ ..........______ 000
+
+00000 ______ DRAGON______ 00000
+
+0000000_________________0000000
+
+000000_________0_________000000
+
+00000_______0000000_______00000
+
+0000_____0000000000000_____0000
+
+000___0000000000000000000___000
+
+00_0000000000000000000000000_00 \e[1;97m
+
+╒══════════════════⭐══════════════════╕\e[1;96m
+
+    FACEBOOK   : TECHNOLOGY BY M4RMU
+
+     AUTHOR    : M4RMU
+
+     GITHUB    : B4BY DR4G0N 
+
+    TELEGRAM   : TERMUX MY4NM4R TEAM　\e[1;97m
+
+╘═══════════🍁═════════════🍁══════════╛
+
+"
+
+echo
+
+echo -e "\e[1;35m[+] \e[1;36m NOW ! YOU SELECT & ENTER => S1 (OR) S2 "
+
+read -p "You Choose Keyboard Style1 or Style2 ? (S1/S2) : " marmu \e[1;32m
+
+if [ $marmu = S1 ]
+
+then
+
+sleep 1
+
+cd $HOME
+
+mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1      
+
+fi
+
+if [ $marmu = S2 ]
+
+then
+
+sleep 1
+
+cd $HOME
+
+mkdir $HOME/.termux/ ;echo "extra-keys = [['F1','F2','F3','F4','F5','F6','F12'],['ESC','TAB','CTRL','ALT','-','DOWN','UP']]" >> $HOME/.termux/termux.properties && termux-reload-settings && sleep 1 
+
+fi
+
+echo
+
+apt update && apt upgrade -y
+
+pkg install nano -y
+
+echo " "
+
+sleep 0.2
+
+clear
+
+sleep 0.2
+
+figlet -f slant  '  WE ' | lolcat
+
+figlet -f slant '  ARE   ' | lolcat
+
+figlet -f slant ' HACKER ' | lolcat
+
+echo "  REBOOT YOUR TERMUX APP (OR) TAKE A NEW SESSION" | lolcat
+
+echo
+
+sleep 0.2
+
+echo " "
+
+xdg-open https://www.facebook.com/marmu.007/
+
+cd $HOME
+
+sleep 0.2
+
+if [ $marmu = 0 ]
+
+then
+
+sleep 1
+
+cd $HOME
+
+exit
+
+fi
